947fe4f480b7c2f79380310fc167e60eebd11cfa
Vladimir Kerkhoff <vladimir.kerkhoff@pon.com>
2019-10-25 10:20:42 +0200

    Initial setup

---
+++ precache-manifest.0a5cd35fa9f82d934f1e79015729bd16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e99133a0875491361289aebd07671c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f6044dfbdee52cc846d",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "58387abc80dd1f0c06c8",
+    "url": "/static/js/2.b74df240.chunk.js"
+  },
+  {
+    "revision": "0f6044dfbdee52cc846d",
+    "url": "/static/js/main.4acf778a.chunk.js"
+  },
+  {
+    "revision": "2ef3b1e4ee435bcd6a1f",
+    "url": "/static/js/runtime-main.bef07d2c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
